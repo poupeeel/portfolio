238a970af39b82db43e16c93a63fa9b0c1c054a8
ayoub boubalgha <126095999+poupeeel@users.noreply.github.com>
2025-05-26 17:51:12 +0100

    ddd

--- next.config.js
+++ next.config.js
@@ -3,34 +3,22 @@
  * for Docker builds.
  */
 await import("./src/env.js");
-import WithPWA from "next-pwa";
-
-const withPWA = WithPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  scope: "/portfolio",
-  sw: "service-worker.js",
-});
 
 /**
  * @type {import('next').NextConfig}
  */
-// @ts-ignore
-const config = withPWA({
+const config = {
   reactStrictMode: true,
- typescript: {
+  output: 'export', // Explicit static export
+  typescript: {
     ignoreBuildErrors: true,
   },
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-});
+  // Remove trailingSlash if you don't need it
+  trailingSlash: true, // Often needed for GitHub Pages
+};
 
 export default config;
